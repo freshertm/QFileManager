fad321acaf9b672fd434924d6da76033dc8770ec
Mikhail.Tiukavkin <mikhail.tiukavkin@neulion.com>
2017-12-28 19:15:59 +0700

    Add direcotory walk using mouse dblclick.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    left = new QFileSystemModel(this);
+    left->setRootPath("C:/Windows");
+    left->setFilter(QDir::NoDot | QDir::AllEntries);
+    ui->leftPanel->setModel(left);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_leftPanel_doubleClicked(const QModelIndex &index)
+{
+    QString path = left->filePath(index);
+    left->setRootPath(path);
+    ui->leftPanel->setRootIndex(left->index(path));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileSystemModel>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_leftPanel_doubleClicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
+
+   QFileSystemModel *left;
+   QFileSystemModel *right;
+
 };
 
 #endif // MAINWINDOW_H
